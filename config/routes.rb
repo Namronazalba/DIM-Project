17162b0b36b83ba6d9264d137822042af15e6bff
Norman.Ablaza <namronazalba@gmail.com>
2022-07-20 17:11:35 +0800

    add disaster to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => "posts#home"
 
-  # resources :posts do
-  #   resources :comments
-  # end
   resources :posts do
     post 'check'
     resources :comments
   end
-
+  resources :disasters
 
 end
